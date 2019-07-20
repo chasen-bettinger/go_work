e7666e1a75bbe273b5cfe8c152661a1b6e62e277
Chasen <bettingerchasen@gmail.com>
2019-07-19 18:14:39 -0600

    CRB:Completed day 3 exercise

--- gigasecond/gigasecond.go
+++ gigasecond/gigasecond.go
@@ -5,8 +5,9 @@ package gigasecond
 // import path for the time package from the standard library
 import "time"
 
+var duration, _ = time.ParseDuration("1000000000s")
+
 // AddGigasecond should have a comment documenting it.
 func AddGigasecond(t time.Time) time.Time {
-
-	return t
+	return t.Add(duration)
 }
