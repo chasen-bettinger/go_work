03e131e89e06f06a470e26afbe858f8b557d4ae1
Chasen <bettingerchasen@gmail.com>
2019-07-20 18:23:38 -0600

    CRB: updated leap code

--- leap/leap.go
+++ leap/leap.go
@@ -1,11 +1,7 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
 // Package leap should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package leap
 
-// IsLeapYear should have a comment documenting it.
+// IsLeapYear evaluates whether a given year is a leap year
 func IsLeapYear(val int) bool {
 	var evenBy4 = val % 4 == 0 
 	if (!evenBy4) { 
